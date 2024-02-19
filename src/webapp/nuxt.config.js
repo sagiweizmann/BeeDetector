166cd9a0271f080ea030f834418152aa94ab7c33
sagiweizmann <sagiweizmann@gmail.com>
2024-02-19 11:34:10 +0200

    Removed unnecessary linter

--- src/webapp/nuxt.config.js
+++ src/webapp/nuxt.config.js
@@ -43,10 +43,7 @@ export default {
    ** Nuxt.js dev-modules
    */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module',
+
   ],
 
   publicRuntimeConfig: {
