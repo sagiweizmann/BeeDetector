4930cc4ed28c2d4625e7454b78d67120cc1da360
sagiweizmann <sagiweizmann@gmail.com>
2024-03-01 17:00:47 +0200

    Fully Hebrew

--- src/webapp/locales/he.js
+++ src/webapp/locales/he.js
@@ -1,53 +1,56 @@
 export default {
-  // כל התרגומים שאתה משתמש במקומות שונים.
   common: {
     email: {
-      label: 'דוא"ל',
-      label_required: 'דוא"ל *',
-      placeholder: 'הזן את כתובת הדוא"ל שלך',
+      label: 'אימייל',
+      label_required: 'אימייל *',
+      placeholder: 'הזינו את כתובת האימייל שלכם',
     },
     submit: 'שלח',
     retry: 'נסה שוב',
     create: 'צור',
     update: 'עדכן',
+    upload : 'העלה',
+    analyze : 'נתח את הוידאו',
+    download_video : 'הורד את הוידאו',
+    download_csv : 'הורד קובץ CSV',
     edit: 'ערוך',
     delete: 'מחק',
     confirm: 'אשר',
-    cancel: 'בטל',
-    send_email: 'שלח דוא"ל',
+    cancel: 'ביטול',
+    send_email: 'שלח אימייל',
     browse: 'עיין',
-    reset: 'אפס',
-    search: 'חפש',
-    export: 'יצא',
+    reset: 'אתחל',
+    search: 'חיפוש',
+    export: 'יצוא',
     all: 'הכל',
     multiple_files: {
-      placeholder: 'בחר קבצים',
-      drop_placeholder: 'זרוק קבצים',
+      placeholder: 'בחרו קבצים',
+      drop_placeholder: 'שחררו קבצים',
     },
     single_file: {
-      placeholder: 'בחר קובץ',
-      drop_placeholder: 'זרוק קובץ',
+      placeholder: 'בחרו קובץ',
+      drop_placeholder: 'שחררו קובץ',
     },
     user: {
       first_name: {
         label: 'שם פרטי',
         label_required: 'שם פרטי *',
-        placeholder: 'הזן שם פרטי',
+        placeholder: 'הזינו שם פרטי',
       },
       last_name: {
         label: 'שם משפחה',
         label_required: 'שם משפחה *',
-        placeholder: 'הזן שם משפחה',
+        placeholder: 'הזינו שם משפחה',
       },
       locale: {
         label: 'אזור',
         label_required: 'אזור *',
-        select: 'בחר אזור',
+        select: 'בחרו אזור',
       },
       role: {
         label: 'תפקיד',
         label_required: 'תפקיד *',
-        select: 'בחר תפקיד',
+        select: 'בחרו תפקיד',
         administrator: 'מנהל',
         user: 'משתמש',
       },
@@ -55,9 +58,11 @@ export default {
     },
     nav: {
       login: 'התחברות',
+      register: 'הרשמה',
       logout: 'התנתקות',
       my_profile: 'הפרופיל שלי',
       dashboard: 'לוח בקרה',
+      welcome_dashboard: 'ברוכים הבאים ללוח הבקרה לניהול',
       administration: 'ניהול',
       users: 'משתמשים',
     },
@@ -65,61 +70,82 @@ export default {
       actions: 'פעולות',
     },
   },
-  // תרגומים של הרכיבים שלך.
+  bee_detector: {
+    title: 'ברוכים הבאים למגלה הדבורים',
+    description: 'זיהוי דבורים בוידאו',
+    market_text: 'מה תרצו לזהות היום?',
+    via_flying: 'זיהוי דבורה מעופפת',
+    via_box: 'זיהוי דבורה בכוורת',
+    download_package : 'הורד חבילת קוד',
+    link_collab : 'קישור לקולאב של Google',
+    faq : 'שאלות נפוצות',
+    contact: 'יצירת קשר',
+    welcome_upload: 'העלו וידאו לזיהוי דבורים',
+    upload: 'העלה וידאו',
+    max_fps : 'מספר תמונות לשנייה',
+    min_move_distance : 'מרחק מינימלי לתנועה',
+    max_move_distance : 'מרחק מקסימלי לתנועה',
+  },
   components: {
     forms: {
       confirm_delete: {
-        enter_confirm: 'הזן "אשר"',
-        danger_zone_message:
-          'אזהרה, סעיף זה מאפשר לך לבצע פעולות מסוכנות ובלתי הפיכות.',
+        enter_confirm: 'הזינו "אישור"',
+        danger_zone_message: 'אזור מסוכן! פעולות בלתי הפיכות עשויות להתרחש כאן.',
       },
     },
   },
-  // תרגומים של התצורות שלך.
   layouts: {
     error: {
       generic_message: 'אירעה שגיאה',
-      not_found_message: 'דף לא נמצא',
-      access_forbidden_message: 'גישה אסורה',
+      not_found_message: 'העמוד לא נמצא',
+      access_forbidden_message: 'גישה נדחתה',
       home_page: 'דף הבית',
     },
   },
-  // תרגומים של הבלנדים שלך.
   mixins: {
     generic_toast: {
       success_message: 'הצלחה 🎉',
     },
   },
-  // תרגומים של הדפים שלך.
   pages: {
     home: {
-      welcome: 'ברוך הבא!',
-      message:
-        'קוד המקור של Symfony מספק יישום מדומה עם מושגים יסודיים ותכליתיים לעזור לך לבנות אפליקציה ווב מודרנית.',
+      welcome: 'ברוכים הבאים!',
+      message: '',
     },
     login: {
       password: {
         label_required: 'סיסמה *',
-        placeholder: 'הזן את הסיסמה שלך',
+        placeholder: 'הזינו את הסיסמה שלכם',
       },
-      error_message: 'הדוא"ל או הסיסמה שסופקו אינם נכונים.',
+      error_message: 'כתובת האימייל או הסיסמה שסופקו אינם נכונים.',
       forgot_password: 'שכחתי את הסיסמה שלי',
     },
     reset_password: {
-      success_message:
-        'אם הכתובת קיימת במערכת שלנו, נשלח אליך דוא"ל עם הוראות לעזרה בשינוי הסיסמה שלך.',
+      success_message: 'אם הכתובת קיימת במערכת שלנו, אז אימייל עם הוראות לשינוי הסיסמה נשלח אליך.',
     },
     update_password: {
       new_password: {
         label_required: 'סיסמה חדשה *',
-        placeholder: 'הזן את הסיסמה החדשה שלך',
+        placeholder: 'הזינו את הסיסמה החדשה שלכם',
       },
       password_confirmation: {
         label_required: 'אימות סיסמה *',
-        placeholder: 'הזן שוב את הסיסמה החדשה שלך',
+        placeholder: 'הזינו שוב את הסיסמה החדשה שלכם',
       },
-      invalid_token_message: 'הטוקן שלך פג או אינו תקף.',
+      invalid_token_message: 'האסימון שלך פג או שאינו תקין.',
       success_message: 'הסיסמה שלך עודכנה בהצלחה.',
+    },
+    register: {
+      password: {
+        label_required: 'סיסמה *',
+        placeholder: 'הזינו את הסיסמה שלכם',
+      },
+      password_repeat: {
+        label_required: 'אימות סיסמה *',
+        placeholder: 'הזינו שוב את הסיסמה שלכם',
+      },
+      error_message: 'כתובת האימייל או הסיסמה שסופקו אינם נכונים.',
+      forgot_password: 'שכחתי את הסיסמה שלי',
     },
   },
 }
